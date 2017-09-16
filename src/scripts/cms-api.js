55b3016f6a5ef12fceea1c54f4456b0ecb330dba
Marie Eriksson <eriksson.km@gmail.com>
2017-09-16 21:32:56 +0200

    split js in two files

---
+++ src/scripts/cms-api.js
@@ -0,0 +1,24 @@
+import Cosmic from 'cosmicjs';
+
+const apiConfig = {
+  bucket: { slug: 'equilab' }
+};
+
+const params = {
+  type_slug: 'hirings',
+  limit: 5,
+  skip: 0
+};
+
+// TODO: check language before api-request
+Cosmic.getObjectType(apiConfig, params, (error, response) => {
+  const objects = response.objects.all;
+  // console.log(objects);
+  objects.forEach(obj => {
+    console.log(obj.title);
+  });
+  // for (let obj in object) {
+  //   console.log(obj.title);
+  // }
+
+});
