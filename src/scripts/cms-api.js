8bc6eb11a7bc6bc703b402b6ab8ce294861e7a1f
Marie Eriksson <eriksson.km@gmail.com>
2017-09-20 22:02:54 +0200

    use find method to get each page.

--- src/scripts/cms-api.js
+++ src/scripts/cms-api.js
@@ -4,26 +4,18 @@ const apiConfig = {
   bucket: { slug: 'equilab' }
 };
 
-const params = {
-  type_slug: 'pages',
-  limit: 5,
-  skip: 0
-};
-
 // TODO: check language before api-request
-Cosmic.getObjectType(apiConfig, params, (error, response) => {
+Cosmic.getObjects(apiConfig, (error, response) => {
   if (error) throw error;
-  const objects = response.objects.all;
-  const hiringpage = objects.map(function(obj) {
-    if (obj.slug === 'landingpage') {
-      console.log(obj);
-    }
+  const pages = response.objects.all;
+  // console.log(objects);
+  const landingpage = pages.find(function (obj) {
+    return obj.slug === 'landingpage';
   });
-  // console.log(hiringpage);
-  // objects.forEach(obj => {
-  //   console.log(obj.title);
-  // });
-  // for (let obj in object) {
-  //   console.log(obj.title);
-  // }
+  document.querySelector('.landingpage-heading').innerText = landingpage.metadata.title_landingpage;
+
+  const hiringpage = pages.find(function (obj) {
+    return obj.slug === 'hiring';
+  });
+  console.log(hiringpage);
 });
