5a9c1cc5137c000785d3dc2dec02cb8f0a6d926f
Marie Eriksson <eriksson.km@gmail.com>
2017-09-21 20:08:36 +0200

    add own fetch method

--- src/scripts/cms-api.js
+++ src/scripts/cms-api.js
@@ -1,4 +1,5 @@
-import Cosmic from 'cosmicjs';
+// import Cosmic from 'cosmicjs';
+import Request from './request';
 
 const apiConfig = {
   bucket: { slug: 'equilab', read_key: 'uL8VNHCHAtT8yniCjW8jFaC90zooc8voQ1qDNVR8krjlKTo310' }
@@ -7,28 +8,26 @@ const apiConfig = {
 // TODO: check language before api-request
 // set class on body and check page - then only fetch current page object?
 
-// function getContentByLanguage (page, language, typeslug) {
-const params = {
-  type_slug: 'pages',
-  slug: 'landingpage', // 'landingpage'
-  locale: 'en-US'// 'en-US' // optional, if localization set on Objects
-};
-// console.log(params);
+function getContentByLanguage (page, language) {
+  const params = {
+    slug: page,
+    locale: language
+  };
 
-Cosmic.getObject(apiConfig, params, (error, response) => {
-  if (error) throw error;
-  console.log(response);
-  // const pages = response.objects.all;
-  // console.log(pages);
-  // console.log(objects);
-  // const landingpage = pages.find(function (obj) {
-  //   return obj.slug === 'landingpage';
-  // });
-  // document.querySelector('.landingpage-heading').innerText = landingpage.metadata.title_landingpage;
+  Request.getObject(apiConfig, params, (error, response) => {
+    if (error) throw error;
+    console.log(response);
+    // const pages = response.objects.all;
+    // console.log(pages);
+    // console.log(objects);
+    // const landingpage = pages.find(function (obj) {
+    //   return obj.slug === 'landingpage';
+    // });
+    // document.querySelector('.landingpage-heading').innerText = landingpage.metadata.title_landingpage;
 
-  // const hiringpage = pages.find(function (obj) {
-  //   return obj.slug === 'hiring';
-  // });
-});
-// }
-// getContentByLanguage('landingpage', 'en-US', 'pages');
+    // const hiringpage = pages.find(function (obj) {
+    //   return obj.slug === 'hiring';
+    // });
+  });
+}
+getContentByLanguage('landingpage', 'sv-SE');
---
+++ src/scripts/request.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const url = 'https://api.cosmicjs.com';
+const apiVersion = 'v1';
+
+module.exports = {
+
+  getObject: function (config, params, callback) {
+    const endpoint = url + '/' + apiVersion + '/' + config.bucket.slug + '/object/' + params.slug + '/?locale=' + params.locale + '&read_key=' + config.bucket.read_key;
+    console.log(endpoint);
+    fetch(endpoint)
+    .then(function (response) {
+      if (response.status >= 400) {
+        var err = {
+          'message': 'There was an error with this request.'
+        };
+        return callback(err, false);
+      }
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  }
+};
