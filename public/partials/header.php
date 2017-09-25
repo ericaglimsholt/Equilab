927cbf34aff89b0eeaff3304ab6d154ef627cabb
Marie Eriksson <eriksson.km@gmail.com>
2017-09-25 09:35:40 +0200

    use php to make partial views

--- public/partials/header.php
+++ public/partials/header.php
@@ -26,15 +26,15 @@
         </li>
         <!-- Input 'FAQ' title from API here -->
         <li class="nav-item">
-          <a class="nav-link" href="/faq.html">FAQ</a>
+          <a class="nav-link" href="/faq.php">FAQ</a>
         </li>
         <!-- Input 'Suggestions' title from API here -->
         <li class="nav-item">
-          <a class="nav-link" href="/suggestionsbox.html">Suggestion Box</a>
+          <a class="nav-link" href="/suggestionsbox.php">Suggestion Box</a>
         </li>
         <!-- Input 'Hiring' title from API here -->
         <li class="nav-item">
-          <a class="nav-link" href="/hiring.html">Work for us</a>
+          <a class="nav-link" href="/hiring.php">Work for us</a>
         </li>
         <!-- Input 'Language' title from API here -->
         <li class="nav-item dropdown">
@@ -47,20 +47,3 @@
       </ul>
     </div>
   </nav>
-
-
-  <!-- Footer -->
-  <div class="footer">
-    <div class="social-media">
-      <img src="/img/socialmedia.png" alt="Equilab Facebook">
-      <img src="/img/socialmedia.png" alt="Equilab Instagram">
-    </div>
-  </div>
-
-
-  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-  <script src="assets/bundle.js"></script>
-</body>
-</html>
