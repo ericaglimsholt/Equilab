bd265dcaccea5efd9919cd57e82386475f66f1ba
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-09-26 15:20:37 +0200

    Added desktop version

--- public/index.php
+++ public/index.php
@@ -1,17 +1,26 @@
 <?php require(__DIR__.'/partials/header.php'); ?>
 
   <!-- Hero content -->
+  <picture>
+    <source srcset="/img/danny-gallegos-desktop.jpg" media="(min-width: 1000px)">
+    <!-- <source srcset="/img/danny_cropped.jpg" media="(min-width: 800px)"> -->
+    <img class="hero-image" srcset="/img/danny-gallegos-desktop.jpg" alt="…">
+  </picture>
   <script id="template-hero-module" type="text/x-handlebars-template">
+
     <div class="row hero-row">
-      <div class="app-links">
-        <h1>{{ title }}</h1>
-        <a href="https://itunes.apple.com/se/app/equilab/id1133163586?utm_campaign=DownloadApp&utm_medium=Website_click&utm_source=Website">
-          <img src="/img/appstore-logo.png" alt="Equilab Apple Store">
-        </a>
-        <a href="https://play.google.com/store/apps/details?id=horse.schvung.equilab&utm_campaign=Download+App&utm_medium=Website&utm_source=Website">
-          <img src="/img/googleplay-logo.png" alt="Equilab Google Play">
-        </a>
-      </div>
+        <div class="col-md-6 title">
+          <h1>{{ title }}</h1>
+        </div>
+        <div class="col-md-6 links">
+          <a href="https://itunes.apple.com/se/app/equilab/id1133163586?utm_campaign=DownloadApp&utm_medium=Website_click&utm_source=Website">
+            <img src="/img/appstore-logo.png" alt="Equilab Apple Store">
+          </a>
+          <a href="https://play.google.com/store/apps/details?id=horse.schvung.equilab&utm_campaign=Download+App&utm_medium=Website&utm_source=Website">
+            <img src="/img/googleplay-logo.png" alt="Equilab Google Play">
+          </a>
+        </div>
+
     </div>
   </script>
   <div class="container hero-module"></div>
@@ -48,7 +57,7 @@
   <div class="containter selling-points-module"></div>
 
   <!-- Image seperator -->
-  <div class="image-seperator">
+  <div class="image-seperator horse">
     <img src="/img/horseriding.png" alt="Equilab is for every horserider">
   </div>
 
@@ -68,7 +77,7 @@
   <div class="container quotes-module"></div>
 
   <!-- Image seperator -->
-  <div class="image-seperator">
+  <div class="image-seperator wireframes">
     <img src="/img/iphoneapp.png" alt="Equilab is for every horserider">
   </div>
 
@@ -79,7 +88,8 @@
       <h2>Used by more than 30 000 riders worldwide!</h2>
       <!-- Include description from API here -->
       <p>We all love horses and wants what’s best for them. This requires us to keep track of our horses and exercise them in a sustainable way. We hope to make this a little bit easier by helping you to automatically track your trainings with the help of your phone. This will help you keep better control over your exercises and make sure you balanced it in the best possible way for your horse & goals.</p>
-      <!-- Include appstore icon from API here -->
+
+    <div class="app-links">
       <a href="https://itunes.apple.com/se/app/equilab/id1133163586?utm_campaign=DownloadApp&utm_medium=Website_click&utm_source=Website">
         <img src="/img/appstore-logo.png" alt="Equilab Apple Store">
       </a>
@@ -87,6 +97,8 @@
       <a href="https://play.google.com/store/apps/details?id=horse.schvung.equilab&utm_campaign=Download+App&utm_medium=Website&utm_source=Website">
         <img src="/img/googleplay-logo.png" alt="Equilab Google Play">
       </a>
+    </div>
+
     </div>
   </div>
 
