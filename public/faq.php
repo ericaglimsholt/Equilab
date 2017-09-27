d1deddd262ee98e750a2ff074c0714acf691e058
Marie Eriksson <eriksson.km@gmail.com>
2017-09-27 15:46:55 +0200

    add faq example

--- public/faq.php
+++ public/faq.php
@@ -5,15 +5,29 @@
 </div>
 
 <div class="faq-module">
-  
+
   <div class="row faq-heading">
     <h1>FAQ</h1>
   </div>
 
   <div class="row faq">
     <div class="col-md-5 categories">
+      <ul>
+        <li><h2>Basics</h2></li>
+        <li><h2>Usage</h2></li>
+        <li><h2>Functionality</h2></li>
+        <li><h2>Privacy</h2></li>
+        <li><h2>Miscellaneous</h2></li>
+      </ul>
     </div>
+
     <div class="col-md-7 questions">
+      <div class="question">
+        <p>How do I start an exercise</p>
+      </div>
+      <div class="answer">
+        <p>Before or when you are on the horse you simply press start activity and you see the clock start ticking. You are now measuring the movements of your horse and can simply go on exercising as you usually do!</p>
+      </div>
     </div>
 
   </div>
