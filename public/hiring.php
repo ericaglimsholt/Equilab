5dabe290527a180e01d0bd87d79219ed8df8bb3f
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-09-28 10:46:32 +0200

    Added markup content to hiring

--- public/hiring.php
+++ public/hiring.php
@@ -1,4 +1,28 @@
 <?php require(__DIR__.'/partials/header.php'); ?>
 
+<div class="hero-image">
+  <img src="/img/minecraft-1920x500.jpg" alt="">
+</div>
+
+<div class="container">
+  <div class="row work">
+    <div class="col-md-12">
+      <h2>Work for us</h2>
+      <h3>We are growing!</h3>
+      <p>Text</p>
+      <h3>This is us</h3>
+      <p>Text</p>
+    </div>
+  </div>
+  <div class="row jobs">
+    <div class="col-md-6">
+      <h3>React Native Developer</h3>
+    </div>
+    <div class="col-md-6">
+      <h3>The task at hand</h3>
+      <p>Text</p>
+    </div>
+  </div>
+</div>
 
 <?php require(__DIR__.'/partials/footer.php'); ?>
--- public/suggestionsbox.php
+++ public/suggestionsbox.php
@@ -1,5 +1,8 @@
 <?php require(__DIR__.'/partials/header.php'); ?>
 
+<div class="hero-image">
+  <img src="/img/suggestionhorse-1920x500.jpg" alt="">
+</div>
   <h1>Suggestions box</h1>
 
 <?php require(__DIR__.'/partials/footer.php'); ?>
