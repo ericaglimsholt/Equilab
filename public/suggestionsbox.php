36b24ef4045bc39f3a213faa2847f85566de8c07
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-02 13:44:32 +0200

    Added content for page

--- public/suggestionsbox.php
+++ public/suggestionsbox.php
@@ -1,8 +1,30 @@
 <?php require(__DIR__.'/partials/header.php'); ?>
 
+<!-- Hero image -->
 <div class="hero-image">
   <img src="/img/suggestionhorse-1920x500.jpg" alt="">
 </div>
-  <h1>Suggestions box</h1>
+
+<!-- Suggestion box -->
+<div class="container">
+  <div class="row introducation">
+    <!-- Introduction text -->
+    <div class="col-md-12">
+      <h2>Suggestion Box</h2>
+      <p>Listed list bla bla bla</p>
+    </div>
+  </div>
+  <div class="row suggestions">
+    <!-- Suggestions -->
+    <div class="col-md-8">
+      <h3>Instagram integration</h3>
+      <p>Text</p>
+    </div>
+    <!-- Counter -->
+    <div class="col-md-4">
+      <p class="counter">877</p>
+    </div>
+  </div>
+</div>
 
 <?php require(__DIR__.'/partials/footer.php'); ?>
