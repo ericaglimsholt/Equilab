d5a8f80f424c8cf2039eb0a890f8802b1ec4b8a8
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-02 14:44:33 +0200

    Added more content

--- public/suggestionsbox.php
+++ public/suggestionsbox.php
@@ -11,18 +11,60 @@
     <!-- Introduction text -->
     <div class="col-md-12">
       <h2>Suggestion Box</h2>
-      <p>Listed list bla bla bla</p>
+      <p>Listed below are suggestions for new functionality that might be added to the app. Please vote for the suggestions you like to see appear in future updates!</p>
     </div>
   </div>
   <div class="row suggestions">
     <!-- Suggestions -->
-    <div class="col-md-8">
+    <div class="col-md-10">
       <h3>Instagram integration</h3>
-      <p>Text</p>
+      <p>Directly after you have stopped exercising you press stopp training, add some extra informationen if you want that. Then all you need to do is press save activity which will make the app save and process the information.</p>
     </div>
     <!-- Counter -->
-    <div class="col-md-4">
+    <div class="col-md-2">
       <p class="counter">877</p>
+    </div>
+  </div>
+  <div class="row suggestions">
+    <!-- Suggestions -->
+    <div class="col-md-10">
+      <h3>Comparision and competition between stables</h3>
+      <p>Directly after you have stopped exercising you press stopp training, add some extra informationen if you want that. Then all you need to do is press save activity which will make the app save and process the information.</p>
+    </div>
+    <!-- Counter -->
+    <div class="col-md-2">
+      <p class="counter">522</p>
+    </div>
+  </div>
+  <div class="row suggestions">
+    <!-- Suggestions -->
+    <div class="col-md-10">
+      <h3>Encourage your friends training</h3>
+      <p>Directly after you have stopped exercising you press stopp training, add some extra informationen if you want that. Then all you need to do is press save activity which will make the app save and process the information.</p>
+    </div>
+    <!-- Counter -->
+    <div class="col-md-2">
+      <p class="counter">249</p>
+    </div>
+  </div>
+  <div class="row suggestions">
+    <!-- Suggestions -->
+    <div class="col-md-10">
+      <h3>Training achievements</h3>
+      <p>Directly after you have stopped exercising you press stopp training, add some extra informationen if you want that. Then all you need to do is press save activity which will make the app save and process the information.</p>
+    </div>
+    <!-- Counter -->
+    <div class="col-md-2">
+      <p class="counter">113</p>
+    </div>
+  </div>
+  <div class="row forms">
+    <div class="col-md-12 text">
+      <h3>Missing a feature?<br/>
+      Please send us your thoughts and ideas!</h3>
+      <div class="send-box">
+        <p class="send">Make a suggestion</p>
+      </div>
     </div>
   </div>
 </div>
