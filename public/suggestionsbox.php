ff200001878506a82c8a1e1be729c1ab4f25611e
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-10-10 14:22:25 +0200

    Changes the images

--- public/suggestionsbox.php
+++ public/suggestionsbox.php
@@ -5,7 +5,7 @@
   <img src="/img/suggestionhorse-1920x500.jpg" alt="">
 </div>
 <!-- Curve -->
-<div class="curve"></div>
+<div class="curve sugg"></div>
 <!-- Suggestion box -->
 <div class="container suggestion-box">
   <div class="row introducation">
