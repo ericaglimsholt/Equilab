5beac8bf4f194599d29427439ba442602312e9e3
Marie Eriksson <eriksson.km@gmail.com>
2017-10-10 17:43:35 +0200

    menu almost working

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,34 +1,35 @@
 <!-- Footer -->
-</div>
-<div class="footer">
-  <div class="container">
-    <div class="row">
-      <!-- Equilab logotype -->
-      <div class="col-md-4 logo">
-        <img class="logotype" src="/img/Equilab_logo_v2_white.png" alt="">
-      </div>
-      <div class="col-md-4 info">
-        <p>Contact us <br/> team@equilab.com</p>
-      </div>
-      <!-- Social media links -->
-      <div class="col-md-4 social-media">
-        <a href="https://www.facebook.com/equilabapp/">
-          <img src="/img/icon_facebook.png" alt="Equilab Facebook">
-        </a>
-        <a href="https://www.instagram.com/equilabapp/">
-          <img src="/img/icon_instragram.png" alt="Equilab Instagram">
-        </a>
-        <div class="terms">
-          <p>Terms & services</p>
-        </div>
 
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <!-- Equilab logotype -->
+        <div class="col-md-4 logo">
+          <img class="logotype" src="/img/Equilab_logo_v2_white.png" alt="">
+        </div>
+        <div class="col-md-4 info">
+          <p>Contact us <br/> team@equilab.com</p>
+        </div>
+        <!-- Social media links -->
+        <div class="col-md-4 social-media">
+          <a href="https://www.facebook.com/equilabapp/">
+            <img src="/img/icon_facebook.png" alt="Equilab Facebook">
+          </a>
+          <a href="https://www.instagram.com/equilabapp/">
+            <img src="/img/icon_instragram.png" alt="Equilab Instagram">
+          </a>
+          <div class="terms">
+            <p>Terms & services</p>
+          </div>
+
+        </div>
       </div>
     </div>
+
   </div>
 
-</div>
+</div> <!-- end container width -->
 
-</div> <!-- close content wrap -->
 <!-- Javascript files -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
--- public/partials/header.php
+++ public/partials/header.php
@@ -17,45 +17,44 @@ function mix($path)
   <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
-<div class="content-wrap">
 
-<!-- Navbar -->
-  <nav class="navbar navbar-toggleable-md navbar-light bg-faded navbar-default sticky-top">
-    <!-- Button when on mobile device -->
-    <img src="">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- Equilab logotype -->
-    <a class="navbar-brand" href="/">
-      <img src="/img/Equilab_logo_v2_white.png" alt="Equilab Icon">
-    </a>
+  <div class="container-width">
 
-    <script id="template-menu-module" type="text/x-handlebars-template">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active" data-page="">
-          <a class="nav-link" href="/">{{ home }} <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item" data-page="faq.php">
-          <a class="nav-link" href="/faq.php">{{ faq }}</a>
-        </li>
-        <li class="nav-item" data-page="suggestionsbox.php">
-          <a class="nav-link" href="/suggestionsbox.php">{{ suggestion-box }}</a>
-        </li>
-        <li class="nav-item" data-page="hiring.php">
-          <a class="nav-link" href="/hiring.php"> {{ hiring }}</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">{{ language }}</a>
-          <ul class="dropdown-menu">
-            <li class="lang-item nav-link" data-locale="en-US" href="#">{{ languages.english }}</li>
-            <li class="lang-item nav-link" data-locale="sv-SE" href="#">{{ languages.swedish }}</li>
-          </ul>
-        </li>
-      </ul>
-    </script>
-    <div class="menu-module collapse navbar-collapse navbar-mobile" id="navbarSupportedContent"></div>
-</nav>
-<!-- <div class="curve header-curve"></div> -->
+  <!-- Navbar -->
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded navbar-default sticky-top">
+      <!-- Button when on mobile device -->
+      <img src="">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Equilab logotype -->
+      <a class="navbar-brand" href="/">
+        <img src="/img/Equilab_logo_v2_white.png" alt="Equilab Icon">
+      </a>
 
-<div class="container-width">
+      <script id="template-menu-module" type="text/x-handlebars-template">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active" data-page="">
+            <a class="nav-link" href="/">{{ home }} <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item" data-page="faq.php">
+            <a class="nav-link" href="/faq.php">{{ faq }}</a>
+          </li>
+          <li class="nav-item" data-page="suggestionsbox.php">
+            <a class="nav-link" href="/suggestionsbox.php">{{ suggestion-box }}</a>
+          </li>
+          <li class="nav-item" data-page="hiring.php">
+            <a class="nav-link" href="/hiring.php"> {{ hiring }}</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">{{ language }}</a>
+            <ul class="dropdown-menu">
+              <li class="lang-item nav-link" data-locale="en-US" href="#">{{ languages.english }}</li>
+              <li class="lang-item nav-link" data-locale="sv-SE" href="#">{{ languages.swedish }}</li>
+            </ul>
+          </li>
+        </ul>
+      </script>
+      <div class="menu-module collapse navbar-collapse navbar-mobile" id="navbarSupportedContent"></div>
+  </nav>
+  <!-- <div class="curve header-curve"></div> -->
